6393eee049a42484597c1e563251319aa8007d89
Mimi Wu <mimiwu@google.com>
2018-05-03 17:12:44 +0800

    drivers: scsi: ufs: Disable clock scaling
    Disable clock scaling to avoid costly workqueue overheads.
    
    Power test results on Blueline:
    [without this change]
      Suspend: 9.75mA
      Idle: 238.26mA
      Camera Preview: 1309.99mA
      Partial Wake Lock: 13.67mA
    [with this change - disable clock scaling]
      Suspend: 9.73mA (-0.21%)
      Idle: 215.87mA (-9.4%)
      Camera Preview: 1181.71mA (-9.79%)
      Partial Wake Lock: 13.85mA (+1.32%)
    
    Bug: 78601190
    Signed-off-by: Mimi Wu <mimiwu@google.com>
    Change-Id: I6addaf6f04ebc628155569ad17dd982d5a12e935
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -2515,7 +2515,6 @@ static void ufs_qcom_set_caps(struct ufs_hba *hba)
 	if (!host->disable_lpm) {
 		hba->caps |= UFSHCD_CAP_CLK_GATING |
 		UFSHCD_CAP_HIBERN8_WITH_CLK_GATING |
-		UFSHCD_CAP_CLK_SCALING |
 		UFSHCD_CAP_WB_WITH_CLK_SCALING |
 		UFSHCD_CAP_AUTO_BKOPS_SUSPEND;
 		hba->caps |= UFSHCD_CAP_AGGR_POWER_COLLAPSE;
